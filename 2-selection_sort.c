421340ccd5055855649d3a27bd4fe912b8670207
a-dabour <ah910905@gmail.com>
2023-07-25 09:45:43 -0700

    edit

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -25,9 +25,12 @@ void selection_sort(int *array, size_t size)
 				min_idx = j;
 			}
 		}
-		tmp = array[i];
-		array[i] = array[min_idx];
-		array[min_idx] = tmp;
-		print_array(array, size);
+		if (array[i] != array[min_idx])
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
 	}
 }
